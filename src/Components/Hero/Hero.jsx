9987e456cb54af3c7c1b98c1436b2f683e332f0c
Prince Singh <126652325+singhprince03@users.noreply.github.com>
2023-03-20 14:36:12 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
           useful and interactive web applications.
         </p>
         <a
-          href='https://bit.ly/3kyjXS3'
+          href='https://bit.ly/3n8YoZe'
           target='_blank'
           rel='noopener noreferrer'
         >
